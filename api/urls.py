c479719f2344e1e3dc8083a174206f4783ace52a
JRParreno <jhonrhayparreno22@gmail.com>
2022-09-20 04:33:32 +0800

    added api season

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
+from season.views import SeasonListView
 from agriculture.views import AgricultureListView, AgricultureTypeListView
 
 app_name = 'api'
 
 urlpatterns = [
+    # Agriculture
     path('agriculture/list', AgricultureListView.as_view(), name='agriculture-list'),
     path('agriculture-type/list', AgricultureTypeListView.as_view(),
          name='agriculture-type-list'),
+
+    # Season
+    path('season/list', SeasonListView.as_view(), name='season-list'),
 ]
